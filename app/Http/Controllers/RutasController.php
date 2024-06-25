a7b923f01dc59957d5ac11dfae3501ff1720bbcd
wapmgit <33326378+wapmgit@users.noreply.github.com>
2024-06-25 18:01:30 -0430

    act archivos

--- app/Http/Controllers/RutasController.php
+++ app/Http/Controllers/RutasController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use App\Models\Clientes;
 use App\Models\Rutas;
 use Carbon\Carbon;
 use DB;
